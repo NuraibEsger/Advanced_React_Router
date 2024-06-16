739b0f1cc70fb5e3f6536afa778629658ab92d71
Nuraib <nuraibaa@code.edu.az>
2024-06-16 22:07:55 +0400

    Building a Multi-Page SPA with React Router

---
+++ src/pages/EventDetail.jsx
@@ -0,0 +1,82 @@
+import { Await, defer, json, redirect, useRouteLoaderData } from "react-router-dom";
+import EventItem from "../components/EventItem";
+import EventsList from "../components/EventsList";
+import { Suspense } from "react";
+
+export default function EventDetailPage() {
+  const {event, events} = useRouteLoaderData('event-detail');
+
+  return (
+    <>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={event}>
+          {(loadedEvent) => <EventItem event={loadedEvent} />}
+        </Await>
+      </Suspense>
+      <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+        <Await resolve={events}>
+          {(loadedEvents) => <EventsList events={loadedEvents} />}
+        </Await>
+      </Suspense>
+    </>
+  );
+}
+
+async function loadedEvent(id) {
+  const response = await fetch('http://localhost:8080/events/' + id);
+
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch details for selected event." },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    const resData = await response.json();
+    return resData.event;
+  }
+}
+
+async function loadedEvents() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch events." },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    const resData = await response.json();
+    return resData.events
+  }
+}
+
+export async function loader({ request, params }) {
+  const id = params.id
+
+  return defer({
+    event: await loadedEvent(id),
+    events: loadedEvents()
+  })
+}
+
+export async function action({ params, request }) {
+  const id = params.id;
+  const response = await fetch("http://localhost:8080/events/" + id, {
+    method: request.method
+  });
+
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch details for selected event." },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    return redirect('/events')
+  }
+}
